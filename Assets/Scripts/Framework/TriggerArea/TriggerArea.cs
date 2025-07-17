1e58b58036b096874c1e01e0cddd8aa136e9b96a
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-07-18 00:41:39 +0200

    Added 2D tiggerArea

--- Assets/Scripts/Framework/TriggerArea/TriggerArea.cs
+++ Assets/Scripts/Framework/TriggerArea/TriggerArea.cs
@@ -7,47 +7,40 @@ using Framework.Extensions;
 
 namespace Framework.TriggerArea
 {
-    [RequireComponent(typeof(BoxCollider))]
-    [RequireComponent(typeof(SphereCollider))]
-    [RequireComponent(typeof(CapsuleCollider))]
-    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(CircleCollider2D))]
+    [RequireComponent(typeof(CapsuleCollider2D))]
     public sealed class TriggerArea : MonoBehaviour
     {
-        private const string FBX_SUFFIX = ".fbx";
-        
-        [SerializeField] private StandardMeshes shapeToUse;
+        [SerializeField] private StandardSprites shapeToUse;
         [SerializeField, Tag] private string tagToTriggerWith = "Player";
         [SerializeField] private TriggerBehaviour behaviour;
         [SerializeField] private bool isOneTimeUse;
-        
+
         [Space(20)]
         [SerializeField] private UnityEvent onEnter = new();
         [SerializeField] private UnityEvent onExit = new();
 
-        private MeshFilter _meshFilter;
-        private BoxCollider _boxCollider;
-        private SphereCollider _sphereCollider;
-        private CapsuleCollider _capsuleCollider;
+        private BoxCollider2D _boxCollider;
+        private CircleCollider2D _circleCollider;
+        private CapsuleCollider2D _capsuleCollider;
 
         private bool _isTriggered;
 
         private void Awake()
         {
-            _boxCollider = GetComponent<BoxCollider>();
-            _sphereCollider = GetComponent<SphereCollider>();
-            _capsuleCollider = GetComponent<CapsuleCollider>();
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _circleCollider = GetComponent<CircleCollider2D>();
+            _capsuleCollider = GetComponent<CapsuleCollider2D>();
 
             _boxCollider.isTrigger = true;
-            _sphereCollider.isTrigger = true;
+            _circleCollider.isTrigger = true;
             _capsuleCollider.isTrigger = true;
-            
-            if (!_meshFilter)
-                _meshFilter = GetComponent<MeshFilter>();
 
-            _meshFilter.mesh = null;
+            UpdateVisuals();
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
             if (behaviour == TriggerBehaviour.EXIT_ONLY
                 || CheckOneTimeUse()
@@ -58,48 +51,45 @@ namespace Framework.TriggerArea
             onEnter?.Invoke();
         }
 
-        private void OnTriggerExit(Collider other)
+        private void OnTriggerExit2D(Collider2D other)
         {
             if (behaviour == TriggerBehaviour.ENTER_ONLY
                 || CheckOneTimeUse()
                 || !other.CompareTag(tagToTriggerWith))
                 return;
-            
+
             _isTriggered = true;
             onExit?.Invoke();
         }
 
-        private void OnValidate() => UpdateMesh();
+        private void OnValidate() => UpdateVisuals();
 
-        private void UpdateMesh()
+        private void UpdateVisuals()
         {
-            _meshFilter = GetComponent<MeshFilter>();
-            _meshFilter.mesh = Resources.GetBuiltinResource<Mesh>(shapeToUse.GetStringValue() + FBX_SUFFIX);
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _circleCollider = GetComponent<CircleCollider2D>();
+            _capsuleCollider = GetComponent<CapsuleCollider2D>();
 
-            _boxCollider = GetComponent<BoxCollider>();
-            _sphereCollider = GetComponent<SphereCollider>();
-            _capsuleCollider = GetComponent<CapsuleCollider>();
-            
             switch (shapeToUse)
             {
-                case StandardMeshes.CUBE:
+                case StandardSprites.SQUARE:
                     _boxCollider.enabled = true;
-                    _sphereCollider.enabled = false;
+                    _circleCollider.enabled = false;
                     _capsuleCollider.enabled = false;
                     break;
-                
-                case StandardMeshes.SPHERE:
+
+                case StandardSprites.CIRCLE:
                     _boxCollider.enabled = false;
-                    _sphereCollider.enabled = true;
+                    _circleCollider.enabled = true;
                     _capsuleCollider.enabled = false;
                     break;
-                
-                case StandardMeshes.CAPSULE:
+
+                case StandardSprites.CAPSULE:
                     _boxCollider.enabled = false;
-                    _sphereCollider.enabled = false;
+                    _circleCollider.enabled = false;
                     _capsuleCollider.enabled = true;
                     break;
-                
+
                 default:
                     throw new ArgumentOutOfRangeException();
             }
