4f619b70f17b95b220492d9a1f1a91f768284f14
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-07-18 14:45:41 +0200

    Added pingpong movement

--- Assets/Scripts/Npc/BasicEnemyMovement.cs
+++ Assets/Scripts/Npc/BasicEnemyMovement.cs
@@ -4,26 +4,36 @@ using Framework.Extensions;
 
 namespace Npc
 {
-    public sealed class BasicEnemy : MonoBehaviour
+    public sealed class BasicEnemyMovement : MonoBehaviour
     {
+        [Header("Stats")]
         [SerializeField] private float speed = 2;
         [SerializeField, Tooltip("Move to the right")] private float moveRange = 3;
+        
+        [Header("Debug")]
         [SerializeField] private Color gizmosColor = Color.red;
+        [SerializeField] private float dotSize = 0.1f;
 
         private Vector3 _startPos;
-        private Vector3 _rangePos;
         
+        private void Start() => _startPos = transform.position;
+
         private void FixedUpdate()
         {
-            // move back and forth between _startPos & _rangePos
+            float pingPong = Mathf.PingPong(Time.time * speed, moveRange);
+            Vector3 newPosition = _startPos + Vector3.right * pingPong;
+            transform.position = newPosition;
         }
 
         private void OnDrawGizmos()
         {
             Vector3 to = transform.position;
             to.AddX(moveRange);
+            
             Gizmos.color = gizmosColor;
             Gizmos.DrawLine(transform.position, to);
+            Gizmos.DrawSphere(transform.position, dotSize);
+            Gizmos.DrawSphere(to, dotSize);
         }
     }
 }
