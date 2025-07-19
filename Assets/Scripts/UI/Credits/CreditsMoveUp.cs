eab4cf310c51f4665a474628dccbf6145078295a
nick-geurds <nickgeurdsdesign@gmail.com>
2025-07-19 16:40:20 +0200

    background toegevoegd level 2

--- Assets/Scripts/UI/Credits/CreditsMoveUp.cs
+++ Assets/Scripts/UI/Credits/CreditsMoveUp.cs
@@ -6,18 +6,17 @@ public class CreditsMoveUp : MonoBehaviour
     [Header("Anim Settings")]
     [SerializeField] private float endPositionY;
     [SerializeField] private float moveUpDuration;
-    [SerializeField] private LeanTweenType type;
 
     [Header("Glitch Settings")]
     [SerializeField] private float glitchAmount;
     [SerializeField] private float glitchDuration;
-    [SerializeField] private LeanTweenType glitchType = LeanTweenType.easeShake;
     
     private Vector3 orgPos;
 
     private float timer = 1;
 
     private bool isGlitching = false;
+    private bool glitchIsReset = false;
     private void Start()
     {
         LeanTween.delayedCall(1f, MoveUp);
@@ -29,20 +28,55 @@ public class CreditsMoveUp : MonoBehaviour
             return;
 
         timer += Time.deltaTime;
+
+
+        if (timer >= .2f && !glitchIsReset)
+        {
+            timer = 0;
+            glitchIsReset = true;
+        }
     }
 
     private void MoveUp()
     {
-        LeanTween.moveLocalY(gameObject, endPositionY, moveUpDuration).setEase(type).setOnComplete(() =>
+
+        LeanTween.moveLocalY(gameObject, endPositionY/2, moveUpDuration).setOnComplete(() =>
         {
             orgPos = transform.localPosition;
-        }).setOnComplete(GlitchCredits);
+            MiniGlitchCredits();
+        });
+    }
+
+    private void MiniGlitchCredits()
+    {
+        LeanTween.value(gameObject, 0, glitchAmount, .05f).setOnUpdate((float val) =>
+        {
+            float x = Random.Range(0, glitchAmount);
+            float y = Random.Range(0, glitchAmount);
+
+            transform.localPosition = orgPos + new Vector3(x, y, 0f);
+        })
+            .setOnComplete(() =>
+            {
+                transform.localPosition = orgPos;
+                MoveUp2();
+            });
+    }
+
+    private void MoveUp2()
+    {
+        LeanTween.moveLocalY(gameObject, endPositionY, moveUpDuration).setOnComplete(() =>
+        {
+            orgPos = transform.localPosition;
+            GlitchCredits();
+        });
     }
 
     private void GlitchCredits()
     {
         timer = 0;
         isGlitching = true;
+
 
         LeanTween.value(gameObject, 0, glitchAmount, glitchDuration).setOnUpdate((float val) =>
         {
@@ -53,6 +87,7 @@ public class CreditsMoveUp : MonoBehaviour
         })
             .setOnComplete(() =>
         {
+            LeanTween.scale(gameObject, Vector3.zero, 0.2f).setEaseInElastic().setDestroyOnComplete(true);
             transform.localPosition = orgPos;
         });
 
