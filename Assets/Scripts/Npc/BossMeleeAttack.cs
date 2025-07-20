34b740e67871496d3431270b3fa54e7153809e52
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-07-20 15:26:42 +0200

    Added boss animations

--- Assets/Scripts/Npc/BossMeleeAttack.cs
+++ Assets/Scripts/Npc/BossMeleeAttack.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Npc
 {
@@ -10,6 +11,7 @@ namespace Npc
         [SerializeField] private float attackDistance = 2f;
         [SerializeField] private float attackSpeed = 5f;
         [SerializeField] private float retreatSpeed = 3f;
+        [SerializeField] private List<GameObject> cables;
 
         private bool _isAttacking;
 
@@ -26,6 +28,9 @@ namespace Npc
             Vector3 startPos = hitbox.position;
             Vector3 direction = (player.position.x < hitbox.position.x) ? Vector3.left : Vector3.right;
             Vector3 targetPos = startPos + direction * attackDistance;
+            
+            UpdateCableRotation(direction);
+            UpdateCableColor(direction, false);
 
             // Attack phase
             hitbox.gameObject.SetActive(true);
@@ -38,6 +43,7 @@ namespace Npc
             }
 
             // Retreat phase
+            UpdateCableColor(direction, true);
             elapsed = 0f;
             while (elapsed < 1f)
             {
@@ -49,6 +55,52 @@ namespace Npc
             hitbox.gameObject.SetActive(false);
             hitbox.position = startPos;
             _isAttacking = false;
+        }
+
+        private void UpdateCableRotation(Vector3 dir)
+        {
+            foreach (GameObject cable in cables)
+            {
+                cable.transform.rotation = Quaternion.Euler(0,0, dir == Vector3.right ? -90 : 90);
+            }
+        }
+
+        private void UpdateCableColor(Vector3 dir, bool isGoingBack)
+        {
+            if (isGoingBack)
+            {
+                if (dir == Vector3.right)
+                {
+                    cables[0].SetActive(false);
+                    cables[1].SetActive(true);
+                    cables[2].SetActive(false);
+                    cables[3].SetActive(false);
+                }
+                else
+                {
+                    cables[0].SetActive(false);
+                    cables[1].SetActive(false);
+                    cables[2].SetActive(false);
+                    cables[3].SetActive(true);
+                }
+                
+                return;
+            }
+            
+            if (dir == Vector3.right)
+            {
+                cables[0].SetActive(true);
+                cables[1].SetActive(false);
+                cables[2].SetActive(false);
+                cables[3].SetActive(false);
+            }
+            else
+            {
+                cables[0].SetActive(false);
+                cables[1].SetActive(false);
+                cables[2].SetActive(true);
+                cables[3].SetActive(false);
+            }
         }
     }
 }
